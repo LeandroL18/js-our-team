33551759964d10f227141caa58bb340067161ae6
LeandroL18 <leandrolipani6@gmail.com>
2023-12-02 14:03:30 +0100

    Creation: array membri

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,36 @@
 // Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+ 
+
+// creazione array di oggetti
+const membri = [
+    {
+        'nome': 'Wayne Barnett',
+        'ruolo': 'Founder & CEO',
+        'immagine': 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        'nome': 'Angela Caroll',
+        'ruolo': 'Chief Editor',
+        'immagine': 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        'nome': 'Walter Gordon',
+        'ruolo': 'Office Menager',
+        'immagine': 'walter-gordon-office-manager.jpg'
+    },
+    {
+        'nome': 'Angela Lopez',
+        'ruolo': 'Social Media Menager',
+        'immagine': 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        'nome': 'Scott Estrada',
+        'ruolo': 'Developer',
+        'immagine': 'scott-estrada-developer.jpg'
+    },
+    {
+        'nome': 'Barbara Ramos',
+        'ruolo': 'Graphic Designer',
+        'immagine': 'barbara-ramos-graphic-designer.jpg'
+    }
+]
